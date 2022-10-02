d628955036c9df872fec940b7f005f6bbd2a0cf8
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2022-10-02 17:26:28 +0800

    2nd commit

---
+++ src/test/java/com/example/greeter/AppTest.java
@@ -0,0 +1,17 @@
+
+package com.example.greeter;
+
+
+
+import com.example.greeter.Greeter;
+import java.io.*;
+
+public class AppTest{
+
+
+
+
+    public static void main(String[] args) {
+        System.println(Greeter.sayHello("Ariasa succes!"));
+    }
+}
